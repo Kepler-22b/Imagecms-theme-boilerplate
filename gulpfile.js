0fe8a9e76697915e2ec5f1595d375e87229a7f9a
Kepler-22b <andriy@siteimage.com.ua>
2016-05-24 23:16:36 +0300

    added css nano and autoprefixer via PostCSS

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,15 @@ var del = require('del');
 var sass = require('gulp-sass');
 var debug = require('gulp-debug');
 var concat = require('gulp-concat');
-var autoprefixer = require('gulp-autoprefixer');
-var cssnano = require('gulp-cssnano');
+//var cssnano = require('gulp-cssnano');
 var remember = require('gulp-remember-history');
 var path = require('path');
 var cached = require('gulp-cached');
+
+/* PostCSS plugins */
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
 
 var paths = {
     dist: 'dist',
@@ -24,18 +28,19 @@ var paths = {
 
 /* Compile CSS styles using Sass and PostCSS */
 gulp.task('css', function(){
+    
+    var processors = [
+        autoprefixer({browsers: ['last 2 versions', 'ie > 8', '> 1%']}),
+        //cssnano()
+    ]
+
     return gulp.src(paths.cssSrcArray)
         .pipe(cached('css'))
         .pipe(debug())
         .pipe(sass().on('error', sass.logError))
         .pipe(remember('css'))
         .pipe(concat('final.min.css'))
-        /*
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions', 'ie > 9', '> 2%']
-        }))
-        .pipe(cssnano())
-        */
+        .pipe(postcss(processors))
         .pipe(gulp.dest(paths.cssDist));
 });
 
