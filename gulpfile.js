cc432e71284016b2c01b02e356309a0fb6e1305d
Kepler-22b <andriy@siteimage.com.ua>
2016-05-30 23:27:48 +0300

    gulp config, optimized finding first char of file name for underscored files

--- gulpfile.js
+++ gulpfile.js
@@ -48,28 +48,31 @@ gulp.task('clear', function () {
     return del(paths.dist);
 });
 
+
 /* Watcher */
 gulp.task('watch', function () {
+
     gulp.watch(paths.cssSrcArray, gulp.series('css'))
-    .on('unlink', function (filepath) {
-        /* Remove deleted file from history to clear it styles */
-        remember.forget('css', path.resolve(filepath));
-    })
-    .on('change', function(filepath){
-        var unixPath = filepath.split('\\').join('/');
-        var fileNamePosition = unixPath.lastIndexOf('/');
-        
-        /* 
-         * Clear history and cache if included file was changed
-         * All Sass files starting with "_" included to other files
-         * And we should rebuild styles to apply changes
-         */
-        if(unixPath[fileNamePosition+1] == '_'){
-            remember.forgetAll('css');
-            delete cached.caches['css'];
-        }
-    });
+        .on('unlink', function (filepath) {
+            
+            /* Remove deleted file from history to clear it styles */
+            remember.forget('css', path.resolve(filepath));
+
+        })
+        .on('change', function(filepath){
+            
+            /* Files starting with "_" rebuild all styles without caching */
+            var fileName = path.basename(filepath);
+            
+            if(fileName[0] == '_'){
+                remember.forgetAll('css');
+                delete cached.caches['css'];
+            }
+
+        });
+
 });
+
 
 /* Browser Sync with Watcher */
 gulp.task('sync', gulp.parallel('watch', function () {
@@ -78,11 +81,11 @@ gulp.task('sync', gulp.parallel('watch', function () {
     var gulpFolderPath = process.cwd();
     var gulpFolderPathArr = gulpFolderPath.split(path.sep).reverse();
     var hostName = gulpFolderPathArr[2];
-
+    
     browserSync.init({
         proxy: hostName
     });
-
+    
     browserSync.watch([paths.dist+'/**/*.*', '**/*.tpl']).on('change', browserSync.reload);
 
 }));
